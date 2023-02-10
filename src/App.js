9eb448dbedc2f51cf7c1c68326d2f658e37f51a9
JordanU <42947912+UJordan@users.noreply.github.com>
2023-02-10 15:14:59 -0800

    App Setup

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+// import { Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import HomePage from './pages/HomePage';
+// import InfoPage from './pages/InfoPage';
+// import ReservationPage from './pages/ReservationPage';
+// import ContactPage from './pages/ContactPage';
+// import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      text
+      {/* <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='INFO' element={<InfoPage />}/>
+        <Route path='RESERVATION' element={<ReservationPage />}/>
+        <Route path='CONTACT' element={<ContactPage />}/>
+      </Routes>
+      <Footer /> */}
     </div>
   );
 }
