fb44484de832ed788a6aca24abf15bb9088f0eb9
JordanU <42947912+UJordan@users.noreply.github.com>
2023-04-20 09:00:00 -0700

    navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { faYoutube, faTwitter, faInstagram, faFacebook, faGithub, faLinkedin, } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
@@ -7,18 +8,34 @@ const Header = () => {
             <div className='pt-3 pb-1' style={{backgroundColor:'#408E91'}}>
                 <ul className="list-unstyled d-flex " style={{flexWrap:'wrap'}}>
                     <div className="mx-auto d-flex">
-                        <li className="me-3 hover-underline-animation" style={{fontSize:"20px", fontWeight:'bold'}}>About</li>
-                        <li className="me-3 hover-underline-animation" style={{fontSize:"20px", fontWeight:'bold'}}>Porfolio</li>
-                        <li className="hover-underline-animation Icon" style={{fontSize:"20px", fontWeight:'bold'}}>Contact</li>
+                        <li className="me-3 hover-underline-animation" style={{fontSize:"20px", fontWeight:'bold'}}>
+                            <Link to="About" spy={true} smooth={true} offset={-150} duration={500}>
+                                About
+                            </Link>
+                        </li>
+                        <li className="me-3 hover-underline-animation" style={{fontSize:"20px", fontWeight:'bold'}}>
+                            <Link to="Portfolio" spy={true} smooth={true} offset={-125} duration={500}>
+                                Porfolio
+                            </Link>
+                        </li>
+                        <li className="hover-underline-animation Icon" style={{fontSize:"20px", fontWeight:'bold'}}>
+                            <Link to="Contact" spy={true} smooth={true} offset={-150} duration={500}>
+                                Contact
+                            </Link>
+                        </li>
                     </div>
                     <div className="mx-auto d-flex" style={{flexWrap:'wrap'}}>
-                        <li className="me-3 hover-underline-animation" icon={faGithub} style={{fontSize:"20px", fontWeight:'bold'}}>
-                            <FontAwesomeIcon className="me-1" icon={faGithub} style={{fontSize:"20px"}} />
-                            GitHub
+                        <li className="me-3 hover-underline-animation" icon={faGithub}  style={{fontSize:"20px", fontWeight:'bold'}}>
+                            <a href='https://github.com/UJordan' style={{ textDecoration: 'none', color:'inherit' }}>
+                                <FontAwesomeIcon className="me-1" icon={faGithub} style={{fontSize:"20px"}} />
+                                GitHub
+                            </a>
                         </li>
                         <li className="me-3 hover-underline-animation" icon={faLinkedin} style={{fontSize:"20px", fontWeight:'bold'}}>
-                            <FontAwesomeIcon className="me-1" icon={faLinkedin} style={{fontSize:"20px"}} />
-                            LinkedIn
+                            <a href='https://www.linkedin.com/in/jordanulves/' style={{ textDecoration: 'none', color:'inherit' }}>
+                                <FontAwesomeIcon className="me-1" icon={faLinkedin} style={{fontSize:"20px"}} />
+                                LinkedIn
+                            </a>
                         </li>
                         <li className="hover-underline-animation" style={{fontSize:"20px", fontWeight:'bold'}}>
                             <i className='fa fa-file me-1' style={{fontSize:"20px"}} />
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,18 +17,18 @@ const HomePage = () => {
                 </div>
                 <div className='terminal-body'>
                     <div style={{marginLeft: '20px', marginRight:'20px', paddingTop: 10}}>
-                        <h4>About Me</h4>
+                        <h4 id="About">About Me</h4>
                         <p className='terminal-content'>I am a web developer who loves building fun websites and mobile applications. I used to work as an accountant and after I found my passion for web development, I decided to make the transition to become a full-time web developer.</p>
                         <h4>My Skills</h4>
                         <p className='terminal-content'>HTML, CSS, JavaScript, REACT, Bootstrap, REACT Router, REACT Native, Android Studio.</p>
                     </div>
                 </div>
             </div>
-        <h1 className='portfolio'>Portfolio Projects</h1>
+        <h1 className='portfolio' id='Portfolio'>Portfolio Projects</h1>
         <div style={{ marginTop: 30}}>
             <Cards />
         </div>
-        <h1 className='portfolio'>Contact Me</h1>
+        <h1 className='portfolio' id='Contact'>Contact Me</h1>
         </>
     );
 };
